cc62c20ece1d31e22bb260b749feefeef722fe4c
Aditya Musunuri <aditya.musunuri@gmail.com>
2023-02-27 17:45:54 +0530

    feat: added error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { page } from '$app/stores'
+</script>
+
+{#if $page}
+	<div class="w-full h-full flex justify-center items-center pt-16 md:pt-20">
+		<div class="text-center space-y-4">
+			<h2>
+				{$page.status}:{#if $page.error} {$page.error.message} {/if}
+			</h2>
+			<p>We're sorry, something went wrong.</p>
+		</div>
+	</div>
+{/if}
