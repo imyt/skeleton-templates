ca481236e8524b8758d717df0e1ca711a0fa86a1
Aditya Musunuri <aditya.musunuri@gmail.com>
2023-02-27 17:44:55 +0530

    feat: added link to main page

--- src/lib/components/CategoryPage/CategoryHeader.svelte
+++ src/lib/components/CategoryPage/CategoryHeader.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+	import IconArrowLeft from 'virtual:icons/tabler/arrow-left'
 	export let category: App.Category
 </script>
+
+<a href="/#main" class="inline-flex items-center gap-1"><IconArrowLeft />Back to all categories</a>
 
 <h1>{category.name}</h1>
