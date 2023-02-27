001c3158eae57dd33a0ef338041936caecb38a84
Aditya Musunuri <aditya.musunuri@gmail.com>
2023-02-27 16:22:08 +0530

    feat: added Comments components

---
+++ src/templates/CommentsHtml/CommentsHtml.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { Avatar } from '@skeletonlabs/skeleton'
+
+	export let data: {
+		postedAt: string
+		body: string
+		author: { name: string; image: string }
+	}
+</script>
+
+<div class="card p-6">
+	<div class="flex gap-4 items-start">
+		<Avatar src={data.author.image} class="flex-none" />
+		<div class="space-y-1">
+			<div class="">{data.author.name}</div>
+			<div class="text-sm opacity-60">{data.postedAt}</div>
+			<div class="pt-4">
+				{@html data.body}
+			</div>
+		</div>
+	</div>
+</div>
