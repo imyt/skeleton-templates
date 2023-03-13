0b28475c0c29210c467d7ca823624457851c1418
Aditya Musunuri <aditya.musunuri@gmail.com>
2023-03-13 15:02:14 +0530

    feat: added line for skeleteon labs

--- src/lib/components/HomePage/HomePage.svelte
+++ src/lib/components/HomePage/HomePage.svelte
@@ -14,6 +14,13 @@
 <div class="relative p-4">
 	<Banner componentsCount={allComponentsCount} />
 </div>
-<div id="main" class="bg-surface-200-700-token p-4">
+<div class="alert variant-filled-tertiary rounded-none">
+	<div class="alert-message mx-auto">
+		Skeleton Templates is not associated with the open source project called Skeleton or Skeleton
+		Labs, LLC
+	</div>
+</div>
+
+<div id="main" class="bg-surface-200-700-token p-10">
 	<CategoriesList groups={CATEGORIES} componentsCountByCategory={data.componentsCountByCategory} />
 </div>
