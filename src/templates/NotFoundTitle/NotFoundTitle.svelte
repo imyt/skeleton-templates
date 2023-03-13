fad34a3221939952441380fa15e800c38daabce9
Aditya Musunuri <aditya.musunuri@gmail.com>
2023-03-13 15:03:00 +0530

    feat: added 404 page

---
+++ src/templates/NotFoundTitle/NotFoundTitle.svelte
@@ -0,0 +1,11 @@
+<div class="text-center space-y-8">
+	<div class="text-9xl font-extrabold opacity-30">404</div>
+	<div class="text-4xl font-bold">You have found a secret place</div>
+	<p>
+		Unfortunately, this is only a 404 page. You may have mistyped the address, or the page has been
+		moved to another URL.
+	</p>
+	<div>
+		<a href="/">Take me back to home page</a>
+	</div>
+</div>
