fbc59d54aa7459ad8c9d330d28ce5ce2d89379fe
Aditya Musunuri <aditya.musunuri@gmail.com>
2023-10-15 16:26:09 +0530

    fix: add forms back

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
 import { join } from 'path'
 import type { Config } from 'tailwindcss'
+import forms from '@tailwindcss/forms'
 
 // 1. Import the Skeleton plugin
 import { skeleton } from '@skeletonlabs/tw-plugin'
@@ -16,6 +17,7 @@ const config = {
 		extend: {}
 	},
 	plugins: [
+		forms,
 		// 4. Append the Skeleton plugin (after other plugins)
 		skeleton({
 			themes: {
